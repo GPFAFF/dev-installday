a381f92d0d08a2ec1b6c4fe75a576f0134528c1f
GPFAFF <greg.pfaff@gmail.com>
2014-10-23 13:40:52 -0500

    adding conversion

---
+++ gold.rb
@@ -0,0 +1,19 @@
+#rubyscript pricing to gold
+
+
+
+def price_of_gold_from_pounds(pounds)
+
+	cost_of_gold = 1336
+	puts"You have"
+	puts pounds
+	puts "pounds"
+	ans = pounds * 16 * cost_of_gold
+	puts "that equals"
+	puts ans
+	puts "dollars"
+
+end
+
+price_of_gold_from_pounds(12)
+
